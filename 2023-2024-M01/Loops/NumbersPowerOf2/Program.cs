cff0ca6969af7469444cb0631f4520fee26ffc7f
mases777 <mases@abv.bg>
2023-11-18 10:15:20 +0200

    M01-Loops6

---
+++ 2023-2024-M01/Loops/NumbersPowerOf2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumbersPowerOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+
+        }
+    }
+}
