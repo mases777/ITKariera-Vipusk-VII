e24017d3450e444532a4ffc561385f7484afa398
mases777 <mases@abv.bg>
2023-11-18 10:00:48 +0200

    M01-Loops3

---
+++ 2023-2024-M01/Loops/LatinLetters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
