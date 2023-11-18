5e5ee2d37eb38bda1a81095bb841427a745ecf0a
mases777 <mases@abv.bg>
2023-11-18 10:05:20 +0200

    M01-Loops4

---
+++ 2023-2024-M01/Loops/SumOfNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
