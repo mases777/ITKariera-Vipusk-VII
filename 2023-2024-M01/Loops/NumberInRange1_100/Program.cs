81f9c0b1114a23b86293d5e85cf1bbf11360506e
mases777 <mases@abv.bg>
2023-11-18 10:22:41 +0200

    M01-Loops7

---
+++ 2023-2024-M01/Loops/NumberInRange1_100/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NumberInRange1_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Еnter a number in the range [1...100]: ");
+            var n = int.Parse(Console.ReadLine());
+            while (n < 1 || n > 100)
+            {
+                Console.WriteLine("Invalid number!");
+                Console.Write("Еnter a number in the range [1...100]: ");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The number is: {n}");
+
+        }
+    }
+}
