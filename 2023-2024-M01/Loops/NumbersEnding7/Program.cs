65bffebe869647fcc4dadaf8758e9103e2dc70aa
mases777 <mases@abv.bg>
2023-11-18 09:58:31 +0200

    M01-Loops2

---
+++ 2023-2024-M01/Loops/NumbersEnding7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumbersEnding7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 1000; i += 10)
+            {
+                Console.Write(i + " ");
+            }
+            
+            
+            
+            //for (int i = 1; i <= 1000; i++)
+            //{
+            //    if (i % 10 == 7)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+        }
+    }
+}
