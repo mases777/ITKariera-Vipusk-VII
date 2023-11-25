73649606bddd313c990cf39fed4aa433b88adde3
mases777 <mases@abv.bg>
2023-11-25 10:02:58 +0200

    M01-Podg04

---
+++ 2023-2024-M01/Podgotovka/Zadacha04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zadacha04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int lego = 0, monopoly = 0, puzzle = 0, robot = 0;
+            for (int i = 0; i < n; i++)
+            {
+                var typeGift = Console.ReadLine();
+                switch (typeGift)
+                {
+                    case "lego": lego++; break;
+                    case "monopoly": monopoly++; break;
+                    case "puzzle": puzzle++; break;
+                    case "robot": robot++; break;
+                }
+            }
+
+            Console.WriteLine($"Lego: {lego}");
+            Console.WriteLine($"Monopoly: {monopoly}");
+            Console.WriteLine($"Puzzles: {puzzle}");
+            Console.WriteLine($"Robots: {robot}");
+        }
+    }
+}
