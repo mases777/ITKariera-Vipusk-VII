1570a3f7bb719b456118e81625f2a459f2f007bb
mases777 <mases@abv.bg>
2023-11-25 09:22:53 +0200

    M01-Podg01

---
+++ 2023-2024-M01/Podgotovka/Zadacha01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
+            double catering = rent * 2;
+            double beverage = catering - (catering * 0.15);
+            double presents = (catering + beverage) / 7;
+            double total = rent + catering + beverage + presents;
+            Console.WriteLine($"{total:f2}");
+
+        }
+    }
+}
