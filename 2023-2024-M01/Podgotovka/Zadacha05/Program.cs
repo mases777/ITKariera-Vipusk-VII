b105db600829fc2d120c48a0497bacb3037dd8d8
mases777 <mases@abv.bg>
2023-11-25 10:21:53 +0200

    M01-Podg05

---
+++ 2023-2024-M01/Podgotovka/Zadacha05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zadacha05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int br = 0;
+            for (int a = 0; a <= 9; a++)
+                for (int b = 0; b <= 9; b++)
+                    for (int c = 0; c <= 8; c += 2)
+                        for (int d = 0; d <= 9; d += 9)
+                            for (int e = 3; e <= 6; e += 3)
+                                if (a + b == n)
+                                {
+                                    Console.Write($"{a}{b}{c}{d}{e} ");
+                                    br++;
+                                }
+            Console.WriteLine($"\nCount of winner numbers: {br}");
+
+        }
+    }
+}
