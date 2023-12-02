1f11081071a12c67d91a3a32b77db36759aec8ae
mases777 <mases@abv.bg>
2023-12-02 10:12:10 +0200

    M01-zad13

---
+++ 2023-2024-M01/Podgotovka/Zadacha13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Zadacha13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var days = int.Parse(Console.ReadLine());
+            var room = Console.ReadLine();
+            var grade = Console.ReadLine();
+
+            var result = 0.0;
+            if (room == "room for one person")
+                result = (days - 1) * 18.0;
+            else if (room == "apartment")
+            {
+                result = (days - 1) * 25.0;
+                if (days < 10) result = result * 0.7;
+                else if (days <= 15) result = result * 0.65;
+                else result = result * 0.5;
+            }
+            else
+            {
+                result = (days - 1) * 35.0;
+                if (days < 10) result = result * 0.9;
+                else if (days <= 15) result = result * 0.85;
+                else result = result * 0.8;
+            }
+            if (grade == "positive")
+                result = result * 1.25;
+            else
+                result = result * 0.9;
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
