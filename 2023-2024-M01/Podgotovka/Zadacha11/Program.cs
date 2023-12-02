016fd9b21dfe6899f1a41283f27cf2408268ccd6
mases777 <mases@abv.bg>
2023-12-02 09:37:24 +0200

    M01-zad11

---
+++ 2023-2024-M01/Podgotovka/Zadacha11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zadacha11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var paper = int.Parse(Console.ReadLine());
+            var cloth = int.Parse(Console.ReadLine());
+            var glue = double.Parse(Console.ReadLine());
+            var percent = int.Parse(Console.ReadLine());
+
+            var totalPaper = paper * 5.8;
+            var totalCloth = cloth * 7.2;
+            var totalGlue = glue * 1.2;
+            var total = totalPaper + totalCloth + totalGlue;
+            var price = total - total * percent / 100.0;
+
+            Console.WriteLine($"{price:f3}");
+        }
+    }
+}
