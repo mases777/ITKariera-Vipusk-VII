5df420b279daf4b2bacecaeaf86c34efe73ce9c5
mases777 <mases@abv.bg>
2023-12-02 10:27:22 +0200

    M01-zad14

---
+++ 2023-2024-M01/Podgotovka/Zadacha14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zadacha14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+
+            while (m % 6 != 0)
+            {
+                m--;
+            }
+            for (int i = m; i >= n; i -= 6)
+            {
+                if (i == s)
+                {
+                    break;
+                }
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
