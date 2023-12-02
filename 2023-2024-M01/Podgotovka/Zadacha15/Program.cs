c49da48d1e1ee0f9a82d440fe6a3ead33572a424
mases777 <mases@abv.bg>
2023-12-02 10:45:22 +0200

    M01-zad15

---
+++ 2023-2024-M01/Podgotovka/Zadacha15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Zadacha15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int a = number % 10;
+            number = number / 10;
+            int b = number % 10;
+            number = number / 10;
+            int c = number;
+
+            for (int m = 1; m <= a; m++)
+                for (int n = 1; n <= b; n++)
+                    for (int p = 1; p <= c; p++)
+                    {
+                        Console.WriteLine($"{m} * {n} * {p} = {m * n * p}");
+                    }
+        }
+    }
+}
