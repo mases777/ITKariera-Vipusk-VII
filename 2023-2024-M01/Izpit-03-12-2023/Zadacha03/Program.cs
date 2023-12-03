12b3def3bc2ee3d6a622b89217945acb0077d2e9
mases777 <mases@abv.bg>
2023-12-03 11:08:45 +0200

    M01-izpit03

--- 2023-2024-M01/Izpit-03-12-2023/Zadacha03/Program.cs
+++ 2023-2024-M01/Izpit-03-12-2023/Zadacha03/Program.cs
@@ -6,7 +6,31 @@ namespace Zadacha03
     {
         static void Main(string[] args)
         {
-            
+            int nights = int.Parse(Console.ReadLine());
+            string destination = Console.ReadLine();
+            string roomType = Console.ReadLine();
+
+            double pricePerNight = 0;
+
+            switch (destination)
+            {
+                case "Maldives":
+                    if (roomType == "beach villa") pricePerNight = 380;
+                    else if (roomType == "apartment") pricePerNight = 450;
+                    break;
+                case "Bora Bora":
+                    if (roomType == "beach villa") pricePerNight = 350;
+                    else if (roomType == "apartment") pricePerNight = 420;
+                    break;
+                case "Singapore":
+                    if (roomType == "beach villa") pricePerNight = 390;
+                    else if (roomType == "apartment") pricePerNight = 490;
+                    break;
+            }
+
+            double totalCost = nights * pricePerNight;
+
+            Console.WriteLine($"They have to spend {totalCost:f2} leva.");
         }
     }
 }
