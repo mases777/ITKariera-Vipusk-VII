af3f9d3dcea2f5d79b782d238769db07fa7261d5
mases777 <mases@abv.bg>
2023-12-03 10:59:09 +0200

    M01-izpit01

---
+++ 2023-2024-M01/Izpit-03-12-2023/Zadacha01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rent = int.Parse(Console.ReadLine());
+            var food = rent * 3.0;
+            var drinks = food * 0.8;
+            var decorations = (food + drinks) * 0.1;
+            var total = rent + food + drinks + decorations;
+            Console.WriteLine($"{total:f2}");
+        }
+    }
+}
---
+++ 2023-2024-M01/Izpit-03-12-2023/Zadacha02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadacha02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
