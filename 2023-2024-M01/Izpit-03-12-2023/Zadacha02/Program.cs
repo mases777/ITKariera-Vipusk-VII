5fc2000c7069661c5e95e0b4d5b868e2a398674c
mases777 <mases@abv.bg>
2023-12-03 11:01:57 +0200

    M01-izpit02

--- 2023-2024-M01/Izpit-03-12-2023/Zadacha02/Program.cs
+++ 2023-2024-M01/Izpit-03-12-2023/Zadacha02/Program.cs
@@ -6,7 +6,24 @@ namespace Zadacha02
     {
         static void Main(string[] args)
         {
-            
+            var tshirt = double.Parse(Console.ReadLine());
+            var alltshirt = int.Parse(Console.ReadLine());
+            var alljacket = int.Parse(Console.ReadLine());
+            var alljeans = int.Parse(Console.ReadLine());
+            var allshirt = int.Parse(Console.ReadLine());
+            var money = double.Parse(Console.ReadLine());
+            var jacket = tshirt * 1.4;
+            var jeans = jacket * 0.8;
+            var shirt = jeans - 10;
+            var total = (tshirt * alltshirt) + (jacket * alljacket) + (jeans * alljeans) + (shirt * allshirt);
+            if (money >= total)
+            {
+                Console.WriteLine($"Yes! {money - total:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"No! {total - money:f2} leva needed.");
+            }
         }
     }
 }
