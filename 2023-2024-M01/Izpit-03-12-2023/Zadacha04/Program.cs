05027c3fb3d1ad4a0969f041602ace08c2ae0889
mases777 <mases@abv.bg>
2023-12-03 11:10:23 +0200

    M01-izpit04

--- 2023-2024-M01/Izpit-03-12-2023/Zadacha04/Program.cs
+++ 2023-2024-M01/Izpit-03-12-2023/Zadacha04/Program.cs
@@ -6,7 +6,33 @@ namespace Zadacha04
     {
         static void Main(string[] args)
         {
-            
+            int materialsCount = int.Parse(Console.ReadLine());
+
+            int tilesCount = 0;
+            int toolsCount = 0;
+            int paintCount = 0;
+
+            for (int i = 0; i < materialsCount; i++)
+            {
+                string materialType = Console.ReadLine();
+
+                switch (materialType)
+                {
+                    case "tile":
+                        tilesCount++;
+                        break;
+                    case "tools":
+                        toolsCount++;
+                        break;
+                    case "paint":
+                        paintCount++;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Tiles: {tilesCount}");
+            Console.WriteLine($"Tools: {toolsCount}");
+            Console.WriteLine($"Paint: {paintCount}");
         }
     }
 }
