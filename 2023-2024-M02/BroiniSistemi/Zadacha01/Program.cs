69afe94a4205d87398bf3fe4ae927f9d34343806
mases777 <mases@abv.bg>
2024-01-13 09:57:41 +0200

    M02-Zad01

---
+++ 2023-2024-M02/BroiniSistemi/Zadacha01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Dec2Bin(n));
+            Console.WriteLine(Dec2Hex(n));
+        }
+
+        static string Dec2Hex(int n)
+        {
+            string result = "";
+            while (n > 0)
+            {
+                int hex = (n % 16) + 48;
+                if (hex > 57) hex += 7;
+                result += (char)hex;
+                n = n / 16;
+            }
+            return StringReverse(result);
+        }
+
+        static string Dec2Bin(int n)
+        {
+            string result = "";
+            while (n > 0)
+            {
+                result += n % 2;
+                n = n / 2;
+            }
+            return StringReverse(result);
+        }
+
+        static string StringReverse(string str)
+        {
+            string result = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                result += str[i];
+            }
+            return result;
+        }
+    }
+}
