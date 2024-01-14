c2d291caff94ecedff1d922359700e6ff7f263cc
mases777 <mases@abv.bg>
2024-01-14 09:27:24 +0200

    M02-T01

---
+++ 2023-2024-M02/TipoveDanni/Zadacha01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var output = Convert.ToInt32(input, 16);
+            Console.WriteLine(output);
+        }
+    }
+}
