cdca296aea12f56822dc48e7b291cbbf2a1f2301
mases777 <mases@abv.bg>
2024-01-14 09:52:19 +0200

    M02-T04

---
+++ 2023-2024-M02/TipoveDanni/Zadacha04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zadacha04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double m = double.Parse(Console.ReadLine());
+            double s = double.Parse(Console.ReadLine());
+
+            double time = h * 3600.0 + m * 60.0 + s;
+
+            Console.WriteLine(Math.Round(meters / time, 6));
+            Console.WriteLine(Math.Round((meters/1000.0) / (time / 3600.0), 6));
+            Console.WriteLine(Math.Round((meters / 1609.0) / (time / 3600.0), 6));
+        }
+    }
+}
