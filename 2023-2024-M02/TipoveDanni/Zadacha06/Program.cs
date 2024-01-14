df2f3991a2a0574e5e3bbffb55e1047adfd3f239
mases777 <mases@abv.bg>
2024-01-14 10:07:47 +0200

    M02-T06

---
+++ 2023-2024-M02/TipoveDanni/Zadacha06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zadacha06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfDigits = 0;
+                int digits = i;
+                while (digits > 0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits /= 10;
+                }
+                bool special = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+
+                Console.WriteLine($"{i} -> {special}");
+            }
+        }
+    }
+}
