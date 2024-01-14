85ef96b40db3e677450fd04b7978f99d05a286bf
mases777 <mases@abv.bg>
2024-01-14 09:35:22 +0200

    M02-T02

---
+++ 2023-2024-M02/TipoveDanni/Zadacha02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zadacha02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dec = int.Parse(Console.ReadLine());
+
+            var hex = Convert.ToString(dec, 16).ToUpper();
+            var bin = Convert.ToString(dec, 2);
+
+            Console.WriteLine(hex);
+            Console.WriteLine(bin);
+        }
+    }
+}
