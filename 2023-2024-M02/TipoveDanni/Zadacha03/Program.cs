39b3d54e590f54f67215dd101a9137069f5f07c9
mases777 <mases@abv.bg>
2024-01-14 09:42:04 +0200

    M02-T03

---
+++ 2023-2024-M02/TipoveDanni/Zadacha03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zadacha03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                sum += a % b;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
