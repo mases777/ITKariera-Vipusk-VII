48284ddab2ca3cfafbaa217a0fbceae76f700a38
Mehmed Ali Mehmed <mases@abv.bg>
2024-02-17 12:00:31 +0200

    Add files via upload

---
+++ 2023-2024-M02/List/Zadacha01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Console.WriteLine(string.Join("; ", nums));
+
+        }
+    }
+}
---
+++ 2023-2024-M02/List/Zadacha02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine()
+                .Split(',').ToList();
+
+            foreach (var item in list)
+            {
+                var parts = item.Trim().Split().ToList();
+                Console.WriteLine($"{parts[1]} {parts[0]}");
+            }
+        }
+    }
+}
