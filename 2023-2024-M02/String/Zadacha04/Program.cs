684f12ddfae0f815d14a6956897669b5e2f94b21
Mehmed Ali Mehmed <mases@abv.bg>
2024-02-18 10:29:36 +0200

    M02-String02

---
+++ 2023-2024-M02/String/Zadacha04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Zadacha04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var cmd = Console.ReadLine()
+                .Split().ToArray();
+
+            var sb = new StringBuilder();
+            sb.Append(input);
+
+            switch (cmd[0])
+            {
+                case "Append":
+                    sb.Append(cmd[1]); break;
+                case "Remove":
+                    var pos = int.Parse(cmd[1]);
+                    var number = int.Parse(cmd[2]);
+                    sb.Remove(pos, number);
+                    break;
+                case "Insert":
+                    var pos1 = int.Parse(cmd[1]);
+                    var text = cmd[2];
+                    sb.Insert(pos1, text);
+                    break;
+                case "Replace":
+                    sb.Replace(cmd[1], cmd[2]);
+                    break;
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ 2023-2024-M02/String/Zadacha05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadacha05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var email = Console.ReadLine();
+
+            var parts = email.Split('@').ToArray();
+            var parts1 = parts[0];
+            var parts2 = parts[1];
+
+            int sum1 = 0, sum2 = 0;
+            foreach (var item in parts1)
+            {
+                sum1 += (int)item;
+            }
+            foreach (var item in parts2)
+            {
+                sum2 += (int)item;
+            }
+
+            if (sum1 - sum2 >= 0)
+            {
+                Console.WriteLine("Call her!");
+            }
+            else
+            {
+                Console.WriteLine("She is not the one.");
+            }
+        }
+    }
+}
